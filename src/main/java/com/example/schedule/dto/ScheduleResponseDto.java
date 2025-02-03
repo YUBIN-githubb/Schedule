5f14c08a4f6d93204fb06a5b1d2f0aa633d57cbe
Moon YuBin <yubin5744@gmail.com>
2025-02-03 15:52:21 +0900

    생성일, 수정일 날짜 들어오도록 수정

--- src/main/java/com/example/schedule/dto/ScheduleResponseDto.java
+++ src/main/java/com/example/schedule/dto/ScheduleResponseDto.java
@@ -1,18 +1,31 @@
 package com.example.schedule.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
-@AllArgsConstructor
+
 public class ScheduleResponseDto {
     private Long id;
     private String task;
     private String author;
     private String password;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate createdAt;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate updatedAt;
+
+    public ScheduleResponseDto(Long id, String task, String author, String password, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.task = task;
+        this.author = author;
+        this.password = password;
+        this.createdAt = createdAt.toLocalDate();  // LocalDate로 변환
+        this.updatedAt = updatedAt.toLocalDate();  // LocalDate로 변환
+    }
 
 }
--- src/main/java/com/example/schedule/repository/JdbcTemplateScheduleRepository.java
+++ src/main/java/com/example/schedule/repository/JdbcTemplateScheduleRepository.java
@@ -8,6 +8,7 @@ import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,15 +26,19 @@ public class JdbcTemplateScheduleRepository implements ScheduleRepository {
         SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
         jdbcInsert.withTableName("schedule").usingGeneratedKeyColumns("id");
 
+        LocalDateTime now = LocalDateTime.now(); // 현재 시간 생성
+
         Map<String, Object> parameters = new HashMap<>();
         parameters.put("task", schedule.getTask());
         parameters.put("author", schedule.getAuthor());
         parameters.put("password", schedule.getPassword());
+        parameters.put("created_at", now);  // created_at 추가
+        parameters.put("updated_at", now);  // updated_at 추가
 
         Number key = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(parameters));
 
         return new ScheduleResponseDto(
                 key.longValue(), schedule.getTask(), schedule.getAuthor(),
-                schedule.getPassword(), schedule.getCreatedAt(), schedule.getUpdatedAt());
+                schedule.getPassword(), now, now);
     }
 }
